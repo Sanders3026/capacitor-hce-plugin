8cf399e4a946c65f7330f9e7d6dd268b9b59bc38
Warat Wongmaneekit <thangman22@users.noreply.github.com>
2024-09-14 18:04:07 +0700

    init project

---
+++ android/src/main/java/com/thanmgan22/plugins/nfchce/HCECapacitorPlugin.kt
@@ -0,0 +1,10 @@
+package com.thanmgan22.plugins.nfchce
+
+import android.util.Log
+
+class HCECapacitorPlugin {
+    fun echo(value: String): String {
+        Log.i("Echo", value)
+        return value
+    }
+}
